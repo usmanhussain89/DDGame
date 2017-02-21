93f0e6669455dda84bc3758885375ac38b1400ee
JESTAIS\fyounis <fyounis@jestais.com>
2017-02-21 11:07:22 -0500

    Sorry this is the work to the of the CHaracter and CaracAttrib class

--- src/soen/game/dd/models/Character.java
+++ src/soen/game/dd/models/Character.java
@@ -1,11 +1,15 @@
 /**
  * This class for creating and modifying all the Character fighter or Monster
+ * Implement  Serializable in the interface so the objects of this class can be saved into file
  * @author fyounis
  */
 
-package game.models;
+package soen.game.dd.models;
 
-public class Character {
+
+import java.io.Serializable;
+
+public class Character implements Serializable {
 
 	private int level;
 	private int abilityScores;
@@ -13,6 +17,15 @@ public class Character {
 	private int hitPoint;
 	private int armorClass;
 	private int attackBonus;
-	private int 
+	private int damageBonus;
+	private int multipleAttacks;
+	private ItemType armore;
+	private ItemType ring;
+	private ItemType helmet;
+	private ItemType boots;
+	private ItemType belt;
+	private ItemType sword;
+	private ItemType shield;
+	
 
 }
