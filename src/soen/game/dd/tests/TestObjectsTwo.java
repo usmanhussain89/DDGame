d078cf6e15e7f342427bcb8a19ac1adf6a8bd43f
JESTAIS\fyounis <fyounis@jestais.com>
2017-03-22 15:10:28 -0400

    fix some bugs

---
+++ src/soen/game/dd/tests/TestObjectsTwo.java
@@ -0,0 +1,148 @@
+package soen.game.dd.tests;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import soen.game.dd.fileio.ItemIO;
+import soen.game.dd.fileio.MapIO;
+import soen.game.dd.models.BullyCharacterBuilder;
+import soen.game.dd.models.Campaign;
+import soen.game.dd.models.Character;
+import soen.game.dd.models.CharacterAttribute;
+import soen.game.dd.models.CharacterBuilder;
+import soen.game.dd.models.DummyGameEngine;
+import soen.game.dd.models.Fighter;
+import soen.game.dd.models.FighterType;
+import soen.game.dd.models.Item;
+import soen.game.dd.models.ItemType;
+import soen.game.dd.models.Map;
+import soen.game.dd.models.TankCharacterBuilder;
+import soen.game.dd.models.WeaponType;
+
+/**
+ * 
+ * @author fyounis
+ *This is the second test class
+ *
+ */
+public class TestObjectsTwo {
+	
+
+		private Map map = new Map(10, 10);
+		private Map map2 = new Map(10, 10);
+		private Map map3 = new Map(10, 10);
+		private Map map4 = new Map(10, 10);
+
+		private Campaign BlackCampaign;
+		private Item redHelmet;
+		private Item crazyHelmet;
+		private Item redArmor;
+		private Item redRing;
+		private Item redBoots;
+		private Item redWeapon;
+		private Item redShield;
+		private Item redBelt;
+		private Item blackBelt;
+		private Item crazyWeapon;
+
+		private Character redFeras;
+		private ArrayList<Item> chest;
+
+		public Map addComponentsToMap(Map map) {
+			Map editMap = map;
+
+			Point point = new Point(1, 1);
+			editMap.setEntryPoint(point);
+
+			point.setLocation(10, 10);
+			editMap.setExitPoint(point);
+
+			point.setLocation(1, 1);
+			editMap.setCharacterPoint(point);
+
+			point.setLocation(5, 5);
+			editMap.setChestPoint(point);
+
+			return editMap;
+
+		}
+
+		@Before
+		public void initialize() {
+			redHelmet = new Item("RedHelmet", ItemType.HELMET, CharacterAttribute.INTELLIGENCE, 1, 1,
+					WeaponType.NotAWeapon);
+			crazyHelmet = new Item("crazyHelmet", ItemType.HELMET, CharacterAttribute.INTELLIGENCE, 5, 5,
+					WeaponType.NotAWeapon);
+			redArmor = new Item("redArmor", ItemType.ARMOR, CharacterAttribute.INTELLIGENCE, 2, 5, WeaponType.NotAWeapon);
+			redRing = new Item("redRing", ItemType.RING, CharacterAttribute.INTELLIGENCE, 2, 5, WeaponType.NotAWeapon);
+			redBoots = new Item("redBoots", ItemType.BOOTS, CharacterAttribute.INTELLIGENCE, 2, 5, WeaponType.NotAWeapon);
+			redWeapon = new Item("redWeapon", ItemType.WEAPON, CharacterAttribute.INTELLIGENCE, 1, 1, WeaponType.MELEE);
+			crazyWeapon = new Item("crazyWeapon", ItemType.WEAPON, CharacterAttribute.INTELLIGENCE, 5, 5, WeaponType.MELEE);
+			redShield = new Item("redShield", ItemType.SHIELD, CharacterAttribute.INTELLIGENCE, 2, 5,
+					WeaponType.NotAWeapon);
+			redBelt = new Item("redBelt", ItemType.BELT, CharacterAttribute.INTELLIGENCE, 1, 1, WeaponType.NotAWeapon);
+			blackBelt = new Item("BlackBelt", ItemType.BELT, CharacterAttribute.INTELLIGENCE, 5, 5, WeaponType.NotAWeapon);
+
+			redFeras = new Character("Feras", "The Greater", FighterType.BULLY, 7, 7, 7, 7, 7, 10, redArmor, redRing,
+					redHelmet, redBoots, redBelt, redWeapon, redShield);
+
+			BlackCampaign = new Campaign();
+			BlackCampaign.setCampaignName("BlackCampaign");
+
+			chest = new ArrayList<Item>();
+			chest.add(crazyHelmet);
+			chest.add(blackBelt);
+		}
+		
+		
+		@Test
+		public void wearing_Items_Should_Correctly_Influence_Character_Abilities_Test2(){
+			
+			
+
+				Character jackTheGreate = new Character("Feras", "The Greater", FighterType.BULLY, 7, 7, 7, 10, 10, 10, redArmor,
+						redRing, redHelmet, redBoots, redBelt, redWeapon, redShield);
+				jackTheGreate.setIntelligenceModifier(2.0);
+				jackTheGreate.setAbilityModifier();
+				jackTheGreate.setAttackBonus();
+				jackTheGreate.setLevel(15);
+
+				
+				
+				int weakerItems = jackTheGreate.getDamageBonus();
+				System.out.println("The attack bonus is: "+weakerItems);
+				System.out.println("the Damage bonus is :"+jackTheGreate.getDamageBonus());
+
+				
+				/*jackTheGreate.setWeapon(crazyWeapon);
+				jackTheGreate.setBelt(blackBelt);
+				jackTheGreate.setHelmet(crazyHelmet);*/
+				jackTheGreate.setAbilityModifier();
+				jackTheGreate.setAttackBonus();
+				jackTheGreate.setLevel(40);
+				
+
+
+				int strongerItems = jackTheGreate.getDamageBonus();
+				
+				System.out.println("the armor bonus is :"+jackTheGreate.getArmorClass());
+
+				System.out.println("The attack bonus is: "+strongerItems);
+				System.out.println("the Damage bonus is :"+jackTheGreate.getDamageBonus());
+
+				
+				
+				assertTrue((strongerItems > weakerItems));
+			
+		}
+
+}
