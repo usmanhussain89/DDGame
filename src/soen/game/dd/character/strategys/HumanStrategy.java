a55ec19afd1b40063ecba8a35ec31960cfa6743f
JESTAIS\fyounis <fyounis@jestais.com>
2017-04-10 13:20:30 -0400

    creates Strategy classes and interface and implements turn() in all of them, add new class memeber Strategy strategy to the character

---
+++ src/soen/game/dd/character/strategys/HumanStrategy.java
@@ -0,0 +1,23 @@
+package soen.game.dd.character.strategys;
+
+/**
+ * This class will implement the Human or player strategy 
+ * @author fyounis
+ *
+ */
+public class HumanStrategy implements Strategy{
+
+	/**
+	 * @author fyounis
+	 * This Method will play the game in its turn
+	 */
+	@Override
+	public void turn() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private boolean frozen = false;
+	private boolean frightening = false;
+
+}
---
+++ src/soen/game/dd/character/strategys/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * this package will contain all Character strategies 
+ * which can be changed at run time. the turn methods of each characters will be 
+ * represent various strategies and a context object whose behavior varies as per its strategy object.
+ */
+/**
+ * @author fyounis
+ *
+ */
+package soen.game.dd.character.strategys;
