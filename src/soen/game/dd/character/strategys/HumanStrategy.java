db74a978ff59335bce560390fb0fbaf49c176664
f_youni <f_youni@MACARONI.ENCS.concordia.ca>
2017-04-10 22:54:53 -0400

    fix conflict

--- src/soen/game/dd/character/strategys/HumanStrategy.java
+++ src/soen/game/dd/character/strategys/HumanStrategy.java
@@ -8,33 +8,25 @@ import soen.game.dd.models.GameEngine;
  * @author fyounis
  *
  */
-public class HumanStrategy implements Strategy{
-	
+public class HumanStrategy implements Strategy {
+
 	GameEngine gameEngine;
 
-	public HumanStrategy(GameEngine ge){
+	public HumanStrategy(GameEngine ge) {
 		this.gameEngine = ge;
 	}
-	
-	/**
-<<<<<<< HEAD
-	 * @author kelbadawi
-	 * This Method will play the game in its turn
-=======
-	 * @author fyounis This Method will play the game in its turn
->>>>>>> branch 'master' of git@github.com:usmanhussain89/DDGame.git
-	 */
+
 	@Override
 	public void turn() {
 		gameEngine.setCharacterMoved(0);
-		while(gameEngine.getCharacterMoved() < 3){
+		while (gameEngine.getCharacterMoved() < 3) {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 	@Override
